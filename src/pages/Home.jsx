4e8780e006b86c8d0c73bc23e49a85ecfd58208d
lavitz <leojimlavi@gmail.com>
2022-04-24 06:59:51 -0500

    http to https geolocation api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -137,7 +137,7 @@ const Home = () => {
   const searchGeolocation = async (text) => {
     try {
       const res = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${text}&limit=0&appid=${API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${text}&limit=0&appid=${API_KEY}`
       );
       setLocation(res.data);
     } catch (err) {
